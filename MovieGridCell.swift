d8e4be1503a7c069195d86ee12047f2e3f1ef086
AllysaYap <87943375+AllysaYap@users.noreply.github.com>
2021-09-25 20:16:36 -0400

    Add files via upload

---
+++ MovieGridCell.swift
@@ -0,0 +1,17 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Allysa Yap on 9/24/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    
+    
+}
